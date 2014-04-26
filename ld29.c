d576cf13dbd8839b1a4fb10b11d523e303b5c868
Michael Walker <mike@barrucadu.co.uk>
2014-04-26 05:12:16 +0100

    Add a static test level, and a simple game loop

--- ld29.c
+++ ld29.c
@@ -1,10 +1,56 @@
 #include <curses.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 #include "level.h"
 #include "mob.h"
 #include "item.h"
 
 int main() {
+
+	/* Simple static level for rendering */
+	Cell hwall = {.baseSymbol = '-',
+				  .occupant   = NULL,
+				  .items      = NULL};
+
+	Cell vwall = {.baseSymbol = '|',
+				  .occupant   = NULL,
+				  .items      = NULL};
+
+	Cell floor = {.baseSymbol = '.',
+				  .occupant   = NULL,
+				  .items      = NULL};
+
+	Cell floor2 = {.baseSymbol = '.',
+				   .occupant   = NULL,
+				   .items      = NULL};
+
+	Mob player = {.level = NULL,
+				  .symbol = '@',
+				  .next = NULL,
+				  .prev = NULL,
+				  .items = NULL};
+
+	Level level = {.next = NULL,
+				   .prev = NULL,
+				   .mobs = &player};
+
+	player.level = &level;
+
+	for(unsigned int y = 0; y < LEVELHEIGHT; y++) {
+		for(unsigned int x = 0; x < LEVELWIDTH; x++) {
+			if(y == 0 || y == LEVELHEIGHT - 1) {
+				level.cells[x][y] = &hwall;
+			} else if (x == 0 || x == LEVELWIDTH - 1) {
+				level.cells[x][y] = &vwall;
+			} else {
+				level.cells[x][y] = &floor;
+			}
+		}
+	}
+	
+	level.cells[15][10] = &floor2;
+	level.cells[15][10]->occupant = &player;
 
   //set up curses
   initscr();
@@ -13,11 +59,34 @@ int main() {
   nonl();
   intrflush(stdscr, FALSE);
   keypad(stdscr, TRUE);
+  curs_set(0);
 
-  addstr("Hello world!");
+  /* Game loop */
+  Level * current_level = &level;
+  //while(true) {
+	  /* Update mobs */
+	  for(Mob * mob = current_level->mobs; mob != NULL; mob = mob->next) {
+		  //mob update
+	  }
+	  
+	  /* Render level */
+	  clear();
+	  for(unsigned int y = 0; y < LEVELHEIGHT; y ++) {
+		  for(unsigned int x = 0; x < LEVELWIDTH; x++) {
+			  if(current_level->cells[x][y]->occupant != NULL) {
+				  mvaddch(y, x, current_level->cells[x][y]->occupant->symbol);
+			  } else {
+				  mvaddch(y, x, current_level->cells[x][y]->baseSymbol);
+			  }
+		  }
+	  }
+  //}
+
+  // Hang for output
   getch();
 
   //end curses
+  curs_set(1);
   nl();
   echo();
   nocbreak();
