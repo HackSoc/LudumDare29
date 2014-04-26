50c65f78ced96e647e84a73f1c7e1fb31be6a71a
Michael Walker <mike@barrucadu.co.uk>
2014-04-26 18:55:04 +0100

    Factor out the miner code, and use it to place the lakes

--- level.c
+++ level.c
@@ -9,56 +9,52 @@
 extern bool quit;
 
 /**
- * Initialises a level.
- * @param level Level to initialise.
+ * (Shallow) Clone a cell and place it in the given position.
+ * Mobs and items in the cell are NOT cloned, those pointers get shared.
+ * @param level The level to place the cell in.
+ * @param x The X coordinate.
+ * @param y The Y coordinate.
+ * @param to_place The cell to clone.
  */
-void build_level(Level * level) {
-	const int NMINERS = 7;
-	const int SPREAD = 100;
-	const int ITERATIONS = 100;
-	int i, m;
-	int minersx[NMINERS], minersy[NMINERS];
+static void place_cell(Level * level,
+					   unsigned int x,
+					   unsigned int y,
+					   Cell * to_place) {
 
-	for(unsigned int y = 0; y < LEVELHEIGHT; y++) {
-		for(unsigned int x = 0; x < LEVELWIDTH; x++) {
-			level->cells[x][y] = xalloc(Cell);
-			if(y == 0 || y == LEVELHEIGHT - 1) {
-				level->cells[x][y]->baseSymbol = '-';
-				level->cells[x][y]->solid = true;
-			} else if (x == 0 || x == LEVELWIDTH - 1) {
-				level->cells[x][y]->baseSymbol = '|';
-				level->cells[x][y]->solid = true;
-			} else {
-				/*fill 99% of the level with rocks and poison water*/
-				if (rand() % 100 == 0) {
-					level->cells[x][y]->baseSymbol = '.';
-					level->cells[x][y]->solid = false;
-				} else if(rand() % 50 == 0) {
-					level->cells[x][y]->baseSymbol = '~';
-					level->cells[x][y]->solid = false;
-				} else {
-					level->cells[x][y]->baseSymbol = '#';
-					level->cells[x][y]->solid = true;
-				}
-			}
-		}
-	}
-	
-	/* generate starting coordinates near the middle and place the stairs */
-	level->startx = 39 + (rand() % 20);
-	level->starty = 4  + (rand() % 10);
-	level->cells[level->startx][level->starty]->baseSymbol = '<';
-	level->cells[level->startx][level->starty]->solid = false;
+	level->cells[x][y]->baseSymbol = to_place->baseSymbol;
+	level->cells[x][y]->solid      = to_place->solid;
+	level->cells[x][y]->occupant   = to_place->occupant;
+	level->cells[x][y]->items      = to_place->items;
+}
 
-	/* have the miners start where the player starts */
-	for(m = 0; m < NMINERS; m++) {
-		minersx[m] = level->startx;
-		minersy[m] = level->starty;
+/**
+ * Mine out the level. All miners get placed in the same starting
+ * coordinates, and then are spread out, before being free to wander
+ * around as they see fit.
+ * @param level The level to mine
+ * @param num_miners The number of miners to place.
+ * @param spread The maximum distance to spread out the miners.
+ * @param iterations The number of steps to mine.
+ * @param to_place The cell to place.
+ */
+static void mine_level(Level * level,
+					   unsigned int num_miners,
+					   unsigned int spread,
+					   unsigned int iterations,
+					   unsigned int startx, unsigned int starty,
+					   Cell * to_place) {
+	unsigned int minersx[num_miners], minersy[num_miners];
+	unsigned int i, m;
+
+	for(m = 0; m < num_miners; m++) {
+		minersx[m] = startx;
+		minersy[m] = starty;
 	}
+
 	/* spread the miners out a little */
-	for(m = 0; m < NMINERS; m++) {
+	for(m = 0; m < num_miners; m++) {
 		int dx = (m % 2) ? -1 : 1;
-		int totalspread = rand() % SPREAD;
+		unsigned int totalspread = rand() % spread;
 		for(i = 0; i < totalspread; i++) {
 			int dy = (rand() % 2) ? -1 : 1;
 			minersx[m] += dx;
@@ -78,16 +74,13 @@ void build_level(Level * level) {
 				minersy[m] = LEVELHEIGHT - 2;
 			}
 
-			level->cells[minersx[m]][minersy[m]]->baseSymbol = '.';
-			level->cells[minersx[m]][minersy[m]]->solid = false;
-			level->cells[minersx[m]-dx][minersy[m]]->baseSymbol = '.';
-			level->cells[minersx[m]-dx][minersy[m]]->solid = false;
-			level->cells[minersx[m]][minersy[m]-dy]->baseSymbol = '.';
-			level->cells[minersx[m]][minersy[m]-dy]->solid = false;
+			place_cell(level, minersx[m], minersy[m], to_place);
+			place_cell(level, minersx[m]-dx, minersy[m], to_place);
+			place_cell(level, minersx[m], minersy[m]-dy, to_place);
 		}
 	}
-	for(i = 0; i < ITERATIONS; i++) {
-		for(m = 0; m < NMINERS; m++) {
+	for(i = 0; i < iterations; i++) {
+		for(m = 0; m < num_miners; m++) {
 			/* pick a random direction */
 			int dx = (rand() % 2) ? -1 : 1;
 			int dy = (rand() % 2) ? -1 : 1;
@@ -108,12 +101,9 @@ void build_level(Level * level) {
 				minersy[m] = LEVELHEIGHT - 2;
 			}
 
-			level->cells[minersx[m]][minersy[m]]->baseSymbol = '.';
-			level->cells[minersx[m]][minersy[m]]->solid = false;
-			level->cells[minersx[m]-dx][minersy[m]]->baseSymbol = '.';
-			level->cells[minersx[m]-dx][minersy[m]]->solid = false;
-			level->cells[minersx[m]][minersy[m]-dy]->baseSymbol = '.';
-			level->cells[minersx[m]][minersy[m]-dy]->solid = false;
+			place_cell(level, minersx[m], minersy[m], to_place);
+			place_cell(level, minersx[m]-dx, minersy[m], to_place);
+			place_cell(level, minersx[m], minersy[m]-dy, to_place);
 		}
 	}
 	
@@ -125,6 +115,83 @@ void build_level(Level * level) {
 		level->endx = minersx[m];
 		level->endy = minersy[m];
 	}
+}
+
+/**
+ * Initialises a level.
+ * @param level Level to initialise.
+ */
+void build_level(Level * level) {
+	/* For mining passageways */
+	const int NMINERS = 7;
+	const int SPREAD = 100;
+	const int ITERATIONS = 100;
+
+	/* For "mining" lakes of poison water */
+	const int NUMLAKES = 3;
+	const int NLAKEMINERS = 1;
+	const int LAKESPREAD = 100;
+	const int LAKEITERATIONS = 5;
+
+	for(unsigned int y = 0; y < LEVELHEIGHT; y++) {
+		for(unsigned int x = 0; x < LEVELWIDTH; x++) {
+			level->cells[x][y] = xalloc(Cell);
+			if(y == 0 || y == LEVELHEIGHT - 1) {
+				level->cells[x][y]->baseSymbol = '-';
+				level->cells[x][y]->solid = true;
+			} else if (x == 0 || x == LEVELWIDTH - 1) {
+				level->cells[x][y]->baseSymbol = '|';
+				level->cells[x][y]->solid = true;
+			} else {
+				/*fill 99% of the level with rocks*/
+				if (rand() % 100 == 0) {
+					level->cells[x][y]->baseSymbol = '.';
+					level->cells[x][y]->solid = false;
+				} else {
+					level->cells[x][y]->baseSymbol = '#';
+					level->cells[x][y]->solid = true;
+				}
+			}
+		}
+	}
+	
+	/* generate starting coordinates near the middle */
+	level->startx = 39 + (rand() % 20);
+	level->starty = 4  + (rand() % 10);
+
+	/* Mine out passageways */
+	Cell floor = {
+		.baseSymbol = '.',
+		.solid = false,
+		.occupant = NULL,
+		.items = NULL};
+
+	mine_level(level,
+			   NMINERS, SPREAD, ITERATIONS,
+			   level->startx, level->starty,
+			   &floor);
+
+	/* Mine out lakes */
+	Cell poison_lake = {
+		.baseSymbol = '~',
+		.solid = false,
+		.occupant = NULL,
+		.items = NULL};
+
+	int lakes = rand() % NUMLAKES;
+	for(int lake = 0; lake < lakes; lake++) {
+		int lx = rand() % LEVELWIDTH;
+		int ly = rand() % LEVELHEIGHT;
+
+		mine_level(level,
+				   NLAKEMINERS, LAKESPREAD, LAKEITERATIONS,
+				   lx, ly,
+				   &poison_lake);
+	}
+
+	/* Place the stairs */
+	level->cells[level->startx][level->starty]->baseSymbol = '<';
+	level->cells[level->startx][level->starty]->solid = false;
 }
 
 /**
